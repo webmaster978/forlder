090755f18f4dba0ce7bc09995d4b4f10553d0248
webmaster978 <jtjoel11@gmail.com>
2021-04-07 03:34:49 +0200

    ajout

--- upload.php
+++ upload.php
@@ -2,7 +2,7 @@
 if ($_FILES["upload_file"]["name"] != '') {
     $data = explode(".", $_FILES["upload_file"]["name"]);
     $extension = $data[1];
-    $allowed_extension = array("jpg", "png", "gif");
+    $allowed_extension = array("jpg", "png", "gif", "pdf", "mp4");
     if (in_array($extension, $allowed_extension)) {
         $new_file_name = rand() . '.' . $extension;
         $path = $_POST["hidden_folder_name"] . '/' . $new_file_name;
